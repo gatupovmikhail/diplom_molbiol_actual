0f5d6511152a2caf768ff306f94f62b55b131c7b
gatupovmikhail <Porog.97@mail.ru>
2021-03-18 18:22:04 +0300

    добавлено find_din_1 в statistic_of_predictions.py. То есть осуществлен подсчет по попаданию в динукл GT для 1 сайта

--- statistic_of_predictions.py
+++ statistic_of_predictions.py
@@ -7,6 +7,32 @@ def in_ss_pos(mut_gen,mut_ss,pol_gen): # система координат -2-1+
     if mut_ss == 0:
         return 10**4
     return mut_ss + delta
+
+def find_din_1(din,p_ref,p_alt,pos_ss_ref,pos_ss_alt,pos_mut,pos_snp,name_file):
+    ## ['mpTA','pmTA','GmpA','GpmA','GGmp','GGpm','SNP in GT','other'] #GGTA [(-1,-1),(1,1),(1,-1),(2,1),(2,-1),(3,1)] len_label=8
+    pos_dist = [(-1, -1), (1, 1), (1, -1), (2, 1), (2, -1), (3, 1)] # len = 6
+    dist = pos_mut - pos_snp
+    pos_ss_mut = 'NaN'
+    if p_alt - p_ref >= 0.5:
+        pos_ss_mut = pos_ss_alt
+    else:
+        pos_ss_mut = pos_ss_ref
+
+    fl_site = 0
+    for (k, (ps, delta)) in enumerate(pos_dist):
+        if pos_ss_mut== ps and dist == delta:
+            din[k] += 1
+            fl_site = 1
+            break
+
+    if fl_site == 0:
+        pos_ss_snp = in_ss_pos(pos_mut,pos_ss_mut,pos_snp)
+        if pos_ss_snp == 0 or pos_ss_snp  == 1:
+            din[6]+=1
+            fl_site+=1
+    if fl_site == 0:
+        din[7]+=1
+
 def chek(st_ref,st_alt,pos,pos_snp,probab_ref,probab_alt,positions_alt,er):
     fls = [-10]
     for i in [2,3]:
@@ -47,10 +73,14 @@ out_don.write(zag)
 stat = [0]*4
 lab_stat = ['ds_dg','ds_dl','ds_dgdl','all_ds']
 f_pos = 0
+din = [0] * 8 # статистика для динуклеотидов
+din_label = ['mpTA', 'pmTA', 'GmpA', 'GpmA', 'GGmp', 'GGpm', 'SNP in GT', 'other']  # заголовок
+
 for st in pred:
     f_pos +=1
     st_ref = st
     stm = st[:-1].split('\t')
+    pos_mut = int(stm[2])
     predict_ref = stm[6].split('|')
     snp = stm[5].split('|')
     pos_snp = int(snp[2])
@@ -92,14 +122,18 @@ for st in pred:
         print('predict_ref\tpredict_alt\tpos_snp\tsnp')
         print('{}\t{}\t{}\t{}'.format(predict_ref,predict_alt,pos_snp,snp))
         print('###################################################################################################\n')
+
     # проверка позиций прошла успешно
     #ALLEL|GEN|DS_AG|DS_AL|DS_DG|DS_DL|DP_AG|DP_AL|DP_DG|DP_DL)
+
     if (abs(probab_alt[2] - probab_ref[2]) >= 0.5) and not(abs(probab_alt[3] - probab_ref[3]) >= 0.5):
         out_dg.write(st_ref+st_alt)
         stat[0]+=1
+        find_din_1(din, probab_ref[2], probab_alt[2], positions_ref[2], positions_alt[2], pos_mut, pos_snp, 'pred_filtr_ds_dg.vcf')
     if not(abs(probab_alt[2] - probab_ref[2]) >= 0.5) and (abs(probab_alt[3] - probab_ref[3]) >= 0.5):
         out_dl.write(st_ref+st_alt)
         stat[1]+=1
+        find_din_1(din, probab_ref[3], probab_alt[3], positions_ref[3], positions_alt[3], pos_mut, pos_snp,'pred_filtr_ds_dl.vcf')
     if (abs(probab_alt[2] - probab_ref[2]) >= 0.5) and (abs(probab_alt[3] - probab_ref[3]) >= 0.5):
         out_dgdl.write(st_ref+st_alt)
         stat[2]+=1
@@ -110,11 +144,18 @@ print('Статистика')
 print(lab_stat)
 print(stat)
 print('Сумма==all_ds?: {}'.format(sum(stat[:-1])))
-
+print('Статистика по GT. Пока только для предск. с разницей в одном СС.')
+for di,la in zip(din,din_label):
+    print('{}:{}\t'.format(la,di),end='')
+    print()
+print('sum(din)')
+print(sum(din))
+print('sum(stat[0:2])')
+print(sum(stat[0:2]))
 
 
 out_don.close()
-
+## непонятно зачем.
 dinpol = open('polim_in_dinucl.vcf','r')
 zag = dinpol.readline()
 for st in dinpol:
