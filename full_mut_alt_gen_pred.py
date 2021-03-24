717e62a78813fa3a51b40da422e9f3735b0105b4
gatupovmikhail <Porog.97@mail.ru>
2021-03-24 17:04:37 +0300

    Доработка программы score_of_mutations.py none_imp для full_mut_alt_gen_pred.py

--- full_mut_alt_gen_pred.py
+++ full_mut_alt_gen_pred.py
@@ -1,4 +1,7 @@
 from time import time
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.ticker as ticker
 t1 = time()
 import os  # ctrl + alt+ 7
 def func_sort_chr(name):
@@ -14,8 +17,14 @@ def func_sort_pos(name):
 path = '/home/gatupov/PycharmProjects/first_project/Predictions on whole alternative genome/archives'
 files = os.listdir(path)
 files.sort(key=lambda x: (func_sort_chr(x),func_sort_pos(x)) )
-for i in range(len(files)):
-    files[i] = path + '/' + files[i]
-    print(files[i])
+# for i in range(len(files)):
+#     files[i] = path + '/' + files[i]
+#     print(files[i])
 t2 = time()
 print(t2-t1)
+x = [round(el,2) for el in np.arange(0.,1.,0.01)]
+print(x)
+y = [round(el,2) for el in np.arange(0.,1.,0.01)]
+plt.subplots(figsize=(15,15))
+plt.plot(x,y)
+plt.show()
--- score_of_mutations.py
+++ score_of_mutations.py
@@ -1,58 +1,86 @@
 ## Построение гистограммы. Для предсказания каждого полиморфизма выбирается максимальная вероятность образования/нарушения СС.Строится гистограмма
 ## распределения предсказаний по максимальным вероятностям.
 import matplotlib.pyplot as plt
-file_mut = open('mutations_alt_genom_filtr.vcf','r')
-zag = file_mut.readline()
-freq = {}
-for i in range(100):
-    if ((i+1)%10 == 0):
-        freq[str((i+1)/100)+'0']=0
-        continue
-    freq[str((i+1)/100)]=0
-#print(freq.keys())
-for st in file_mut:
-    stm = st[0:-1].split('\t')
-    fl = 0
-    try:
-        info = stm[4]
-    except IndexError:
-        continue
-    try:
-        spliceai = info.split(';')[1]
-    except IndexError:
-        continue
-    probab = spliceai.split('|')
-    try:
-        re = probab[5]
-    except IndexError:
-        continue
-    mmax = '-1'
-    for j in range(2,6):
-        if float(probab[j]) > float(mmax):
-            mmax = probab[j]
-    freq[mmax]+=1
+import matplotlib.ticker as ticker
+# для создание статистики. Сохраняется в freq_mut_alt_genom.txt
+# file_mut = open('mutations_alt_genom_filtr.vcf','r')
+# zag = file_mut.readline()
+# freq = {}
+# for i in range(100):
+#     if ((i+1)%10 == 0):
+#         freq[str((i+1)/100)+'0']=0
+#         continue
+#     freq[str((i+1)/100)]=0
+# #print(freq.keys())
+# for st in file_mut:
+#     stm = st[0:-1].split('\t')
+#     fl = 0
+#     try:
+#         info = stm[4]
+#     except IndexError:
+#         continue
+#     try:
+#         spliceai = info.split(';')[1]
+#     except IndexError:
+#         continue
+#     probab = spliceai.split('|')
+#     try:
+#         re = probab[5]
+#     except IndexError:
+#         continue
+#     mmax = '-1'
+#     for j in range(2,6):
+#         if float(probab[j]) > float(mmax):
+#             mmax = probab[j]
+#     freq[mmax]+=1
+# pr = []
+# fr = []
+# for k in freq.keys():
+#     pr.append(k)
+#     fr.append(freq[k])
+# #print(pr)
+# #print(fr)
+# file_mut.close()
+# S = sum(fr)
+# frp=[]
+# for el in fr:
+#     frp.append(el/S)
+# # for el in frp:
+# #     print('{:.2}'.format(round(el,2)),end=' ')
+# fif = open('freq_mut_alt_genom.txt','w')
+# fif.write('#MAX_PROBABILITY\tAMOUNT\tFREQUENCY\n')
+# for p,fp in zip(pr,fr):
+#     fif.write('{}\t{}\t{:.2}\n'.format(p,fp,round(fp/S,2)))
+#
+# fif.close()
+fif = open('freq_mut_alt_genom.txt','r')
+zag = fif.readline()
 pr = []
+amount = []
 fr = []
-for k in freq.keys():
-    pr.append(k)
-    fr.append(freq[k])
-#print(pr)
-#print(fr)
-file_mut.close()
-S = sum(fr)
-frp=[]
-for el in fr:
-    frp.append(el/S)
-# for el in frp:
-#     print('{:.2}'.format(round(el,2)),end=' ')
-fif = open('freq_mut_alt_genom.txt','w')
-fif.write('#MAX_PROBABILITY\tAMOUNT\tFREQUENCY\n')
-for p,fp in zip(pr,fr):
-    fif.write('{}\t{}\t{:.2}\n'.format(p,fp,round(fp/S,2)))
+
+for st in fif:
+    stm = st[:-1].split('\t')
+    pr.append(float(stm[0])*100)
+    amount.append(int(stm[1]))
+    fr.append(float(stm[2])*100)
 
 fif.close()
-plt.bar(pr,fr)
+print(pr)
+print(fr)
+
+print(amount)
+fig, ax = plt.subplots(figsize=(15,15))
+#  Устанавливаем интервал основных делений:
+ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
+#  Устанавливаем интервал вспомогательных делений:
+ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
+plt.bar(pr,fr,width = 0.8)
 plt.grid(True)
+
+plt.xlabel('Maximum probability from 4 \n probabilities of appearing/disappearing of \n donor/acceptor sites, %')
+plt.ylabel('Percentage from whole amount of predictions, %')
+plt.title('Probabilities of appearing/disappearing splicing sites')
 plt.show()
 
 
