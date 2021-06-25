dbd62457d2d587389f02e2acfec230f92cff4a7b
gatupovmikhail <Porog.97@mail.ru>
2021-06-25 03:41:51 +0300

    Значительно подробнее статистика, мутации разделены на те что не в ad генах, в ad генах без pli скоров, в ad генах с pli скорами.

--- for_pli_scores.py
+++ for_pli_scores.py
@@ -15,6 +15,7 @@ with open('pli_scores.txt', 'r') as f_pl:
         pli.append(float(stm[-1]))
 print(genes[0:4])
 print(pli[0:4])
+print('Количество генов в списке pli и pli:')
 print(len(genes))
 print(len(pli))
 genes_ad = []
@@ -37,13 +38,12 @@ for i in range(len(genes)):
         count += 1
         genes_ad_pli.append(genes[i])
         pli_ad.append(pli[i])
-
-print('COUNT:')
-print(count)
-print(len(genes_ad_pli))
-print(len(pli_ad))
+print(f'Всего генов AD: {len(genes_ad)}')
+print(f'Количество генов AD, которые имеют pli: {count}')
+#print(len(genes_ad_pli))
+#print(len(pli_ad))
 genes_pred = []
-counter_no = {}
+
 
 
 
@@ -72,9 +72,15 @@ print(mas_bord)
 dict_bord = {}
 
 for border in mas_bord:
+    n_mut = 0
+    not_ad_gens = {}
     count_ad = 0
-    counter = 0
-    mybar = Progressbar(10, 81688 / 2)
+    mut_without_ad = 0
+    all_gens = {}
+    count_gens_ad = {}
+    count_gens_ad_pli = {}
+    count_ad_pli = 0
+    mybar = Progressbar(10, 81688 / 2, name='border '+str(round(border,2))+' ')
     dict_bord[str(border)] = 0
     with open('pred_filtr.vcf', 'r') as f_pred:
         head = f_pred.readline()
@@ -83,24 +89,47 @@ for border in mas_bord:
             stm = st[:-1].split('\t')
             gen = stm[-1].split('|')[1]
             gen = gen.upper()
-            if not(gen in genes_ad_pli): # !!!!!!!!!!!!!!!!!!!!!!!!!!!
-                counter += 1
-                if gen in counter_no.keys():
-                    counter_no[gen] += 1
-                else:
-                    counter_no[gen] = 1
+            if not(gen in all_gens.keys()):
+                all_gens[gen] = 1
             else:
-                count_ad += 1
-                if border < pli_ad[genes_ad_pli.index(gen)] <= border + 0.1:
-                    dict_bord[str(border)] += 1
+                all_gens[gen] += 1
+            if not(gen in genes_ad): # !!!!!!!!!!!!!!!!!!!!!!!!!!!
+                mut_without_ad += 1
+                if gen in not_ad_gens.keys():
+                    not_ad_gens[gen] += 1
+                else:
+                    not_ad_gens[gen] = 1
+            else:
+                if (gen in genes_ad_pli):
+                    count_ad_pli += 1
+                    if not (gen in count_gens_ad.keys()):
+                        count_gens_ad_pli[gen] = 1
+                    else:
+                        count_gens_ad_pli[gen] += 1
+                    if border < pli_ad[genes_ad_pli.index(gen)] <= border + 0.1:
+                        dict_bord[str(border)] += 1
+                else:
+                    count_ad += 1
+                    if not (gen in count_gens_ad.keys()):
+                        count_gens_ad[gen] = 1
+                    else:
+                        count_gens_ad[gen] += 1
 
             st = f_pred.readline()
+            n_mut += 1
 
 count_ad += 1
 print()
-print(f'count ad: {count_ad}')
-print(f'counter: {counter}')
-#for el in counter_no.items():
+
+print(f'Мутации, которые НЕ в ad генах: {mut_without_ad}')
+print(f'Кол-во ГЕНОВ с предсказаниями, НЕ попавших в ad: {len(not_ad_gens.keys())}')
+print(f'count ad, мутации, которые в ad генах БЕЗ pli: {count_ad}')
+print(f'Кол-во генов с предсказаниями, попавших в ad БЕЗ pli : {len(count_gens_ad.keys())}')
+print(f'count ad, мутации, которые в ad генах С pli: {count_ad_pli}')
+print(f'Кол-во генов с предсказаниями, попавших в ad С pli : {len(count_gens_ad_pli.keys())}')
+print(f'Всего генов с предсказаниями: {len(all_gens.keys())}')
+print(f'Всего мутаций: {n_mut}')
+#for el in not_ad_gens.items():
     #print(f'{el[0]} {el[1]}')
 x_pli = []
 y_pli = []
@@ -117,7 +146,15 @@ plt.ylabel('number of mutations')
 plt.title('Location of mutation with different predictions \n in AD genes with different PLI scores')
 plt.show()
 
+#count ad: 4072 в ad-генах с pli-скором
+#counter: 36773 не в ad-генах с pli-скором.
+
 # {'0.9': 8908}
 # {'0.8': 9879}
 # {'0.5': 12157}
 # {'0.3': 13717}
+
+# 1533 - Мутации из списка pli, которые являются ad.
+# 1610 - генов ad.
+
+
